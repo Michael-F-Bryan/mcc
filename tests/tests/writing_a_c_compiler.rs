838671a35a404736aa849cb70b31d97a613f0d7b
Michael-F-Bryan <michaelfbryan@gmail.com>
2025-08-23 01:58:36 +0800

    Execute `python3` rather than rely on `test_compiler.py` being marked as executable

--- tests/tests/writing_a_c_compiler.rs
+++ tests/tests/writing_a_c_compiler.rs
@@ -3,17 +3,23 @@ use std::{
     process::Command,
 };
 
+use anyhow::Context;
+
 #[test]
 fn run_test_compiler() {
     let compiler_driver = PathBuf::from(env!("CARGO_BIN_EXE_test_driver"));
     let test_root = Path::new(env!("CARGO_MANIFEST_DIR")).join("writing-a-c-compiler-tests");
 
-    let tc = test_root.join("test_compiler");
-    let mut cmd = command(&tc);
-    cmd.arg(compiler_driver).arg("--chapter=2");
+    let mut cmd = Command::new("python3");
+    cmd.arg(test_root.join("test_compiler"))
+        .arg(compiler_driver)
+        .arg("--chapter=2");
     println!("Running: {cmd:?}");
 
-    let output = cmd.output().unwrap();
+    let output = cmd
+        .output()
+        .with_context(|| format!("Failed to run: {cmd:?}"))
+        .unwrap();
 
     let stdout = String::from_utf8_lossy(&output.stdout);
     if !stdout.trim().is_empty() {
@@ -30,16 +36,4 @@ fn run_test_compiler() {
     }
 
     assert!(output.status.success());
-}
-
-fn command(test_compiler: &Path) -> Command {
-    if cfg!(target_os = "macos") && cfg!(target_arch = "aarch64") {
-        // On Apple Silicon, we need to run the test compiler through rosetta to
-        // get it to run our x86_64 code
-        let mut cmd = Command::new("arch");
-        cmd.arg("-x86_64").arg(test_compiler);
-        cmd
-    } else {
-        Command::new(test_compiler)
-    }
 }
