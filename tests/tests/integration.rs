1837941eb29112db1aaedd980c59166a26afed50
Michael-F-Bryan <michaelfbryan@gmail.com>
2025-08-22 08:39:14 +0800

    Forgot to check out git submodules in CI

--- tests/tests/integration.rs
+++ tests/tests/integration.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use libtest_mimic::Arguments;
 use std::path::Path;
 
@@ -11,7 +12,7 @@ fn main() -> anyhow::Result<()> {
     let ignored = ["chapter_1::invalid_parse::not_expression"];
     let mut trials = Vec::new();
 
-    for test in tests::discover(&test_root)? {
+    for test in tests::discover(&test_root).context("failed to discover tests")? {
         let ignored = test.chapter > MAX_CHAPTER || ignored.contains(&test.name.as_str());
         let trial = test.trial().with_ignored_flag(ignored);
         trials.push(trial);
